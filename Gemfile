8ede1c5d9e910b9b4c201b6edb176090b6aa0d4e
Ahmad Aulia Fahlevi <aauliafahlefi@gmail.com>
2025-05-14 15:04:39 +0700

    initial commit

--- Gemfile
+++ Gemfile
@@ -4,6 +4,10 @@ source "https://rubygems.org"
 
 # gem "rails"
 
+gem "logger"
+gem "public_suffix", "6.0.2"
+gem "rouge", "4.5.2"
+
 gem "jekyll"
 
 group :jekyll_plugins do
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,9 @@
 function myFunction() {
     alert("Kan dah ku bilang jangan di klik!");
 }
+
+function toggleMenu() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('active');
+}
+
