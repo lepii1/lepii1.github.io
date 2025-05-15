1c2556999d7067d6d496df46f8708f1a5b724647
Ahmad Aulia Fahlevi <aauliafahlefi@gmail.com>
2025-05-15 20:44:37 +0700

    initial commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,3 +7,24 @@ function toggleMenu() {
     sidebar.classList.toggle('active');
 }
 
+  document.addEventListener("DOMContentLoaded", function () {
+    const toggleBtn = document.getElementById("theme-toggle");
+    const iconImg = document.getElementById("theme-icon");
+    const body = document.body;
+
+    const sunIcon = "/assets/images/hidup.png";
+    const moonIcon = "/assets/images/mati.png";
+
+    // Cek preferensi
+    if (localStorage.getItem("theme") === "dark") {
+      body.classList.add("dark");
+      iconImg.src = sunIcon;
+    }
+
+    toggleBtn.addEventListener("click", () => {
+      body.classList.toggle("dark");
+      const isDark = body.classList.contains("dark");
+      localStorage.setItem("theme", isDark ? "dark" : "light");
+      iconImg.src = isDark ? sunIcon : moonIcon;
+    });
+  });
